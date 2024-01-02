87f2e7210000e7795c035e1f8c7f953da7a8a87c
guredora <contact@guredora.com>
2024-01-02 16:11:43 +0900

    test for environment

--- test.py
+++ test.py
@@ -1,9 +1,4 @@
 import os
 import datetime
 
-print(os.environ["GITHUB_ACTIONS"])
-dt = datetime.datetime.fromisoformat(os.environ["COMMIT_TIMESTAMP"])
-print(dt.year)
-print(dt.month)
-print(dt.day)
-print(dt.hour)
+print(os.environ.get("GITHUB_ACTIONS", "false") == "true")
