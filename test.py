0d51720269e828bbb2a7903d69fe6a34bc0de1ab
guredora <contact@guredora.com>
2024-01-02 15:21:07 +0900

    test for timestamp

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 
-print(os.environ["GITHUB_ACTIONS"] == True)
-print(type(os.environ["GITHUB_ACTIONS"]))
+print(os.environ["GITHUB_ACTIONS"])
+print(datetime.datetime.fromisoformat(os.environ["COMMIT_TIMESTAMP"]))
